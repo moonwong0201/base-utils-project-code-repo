807d51dbfbbb199b0cb69bb70179e7441d24ae4b
moonwong0201 <wyy19991227@163.com>
2026-02-22 21:35:19 +0800

    Create config.py

---
+++ projects/06-stock-bi-agent/config.py
@@ -0,0 +1,13 @@
+
+OPENAI_API_KEY = ""
+OPENAI_BASE_URL = ""
+OPENAI_MODEL = "qwen-max"
+OPENAI_VISION_MODEL = "qwen-vl"
+
+
+DB_PATH = "./asset/conversations.db"  # 存储对话的历史状态
+
+DATABASE_URL = "sqlite:///./asset/server.db"  # SQLite 数据库连接地址
+
+TOOL_TOKEN = ""
+STOCK_TOKEN = ""
